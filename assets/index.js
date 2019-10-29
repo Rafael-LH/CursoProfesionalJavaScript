cc79651ce118683d20732be77e228e43972b394e
Rafael Lopez <rafael.iskoat@gmail.com>
2019-10-28 22:19:47 -0600

    serviceWorker part1

--- assets/index.js
+++ assets/index.js
@@ -13,3 +13,19 @@ const player = new MediaPlayer({
 // play es un evento que ya biene con tag html de video
 btnPlayPause.onclick = () => player.play();
 btntoggleMute.onclick = (e) => player.toggleMute(e);
+
+
+// Service Workers
+// Sirven para hacer que nuestras aplicaciones funcionen Offline.
+
+// Muy usados en las Progressive Web Apps (PWA) los ServiceWorkers son una capa que vive entre el navegador y el Internet.
+
+// Parecido a como lo hacen los proxys van a interceptar peticiones para guardar el resultado en cache y la próxima vez que
+// se haga la petición tomar del cache ese resultado.
+
+if ('serviceWorker' in navigator) { // comparamos si tenemos serviceWorker en nuestro navegador
+  navigator.serviceWorker.register('/sw.js').catch((err) => { //aqui lo que estamos haciendo es guardar un archivo que en sw.js
+    // pero este puede fallar y por eso le concatenamos un catch por si falla
+    console.log(err.message);
+  });
+}
